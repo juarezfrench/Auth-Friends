acdefe3c7d9fb384f1a5370095d9140fe9f76bf2
juarezfrench <markedfrench@gmail.com>
2019-10-15 15:27:08 -0500

    initial.

---
+++ auth-friends/src/components/LoginForm.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: '',
+      age:'',
+      email: ''
+
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    // login to retrieve the JWT token
+    // add the token to localstorage
+    // route to /protected (whatever landing page)
+    axiosWithAuth()
+      .post('/api/login', this.state.credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        this.props.history.push('/protected');
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
